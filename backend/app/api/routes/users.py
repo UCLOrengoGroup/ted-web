da7edd99091236dfbe8987212ec3bee0ccb085fb
Ian Sillitoe <ian@sillit.com>
2024-03-18 12:22:49 +0000

    refactor db models

--- backend/app/api/routes/users.py
+++ backend/app/api/routes/users.py
@@ -11,7 +11,7 @@ from app.api.deps import (
 )
 from app.core.config import settings
 from app.core.security import get_password_hash, verify_password
-from app.models import (
+from app.models.db import (
     Item,
     Message,
     UpdatePassword,
