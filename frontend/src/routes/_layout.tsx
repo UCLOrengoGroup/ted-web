8acc0ee552bcf04ca9c5c75a8a9e900b35cfc91d
Ian Sillitoe <ian@sillit.com>
2024-03-19 14:10:01 +0000

    swap sidebar for topbar

--- frontend/src/routes/_layout.tsx
+++ frontend/src/routes/_layout.tsx
@@ -1,8 +1,7 @@
-import { Flex, Spinner } from "@chakra-ui/react"
+import { Flex, Spinner, Stack } from "@chakra-ui/react"
 import { Outlet, createFileRoute, redirect } from "@tanstack/react-router"
 
-import Sidebar from "../components/Common/Sidebar"
-import UserMenu from "../components/Common/UserMenu"
+import Topbar from "../components/Common/Topbar"
 import useAuth, { isLoggedIn } from "../hooks/useAuth"
 
 export const Route = createFileRoute("/_layout")({
@@ -20,17 +19,20 @@ function Layout() {
   const { isLoading } = useAuth()
 
   return (
-    <Flex maxW="large" h="auto" position="relative">
-      <Sidebar />
-      {isLoading ? (
-        <Flex justify="center" align="center" height="100vh" width="full">
-          <Spinner size="xl" color="ui.main" />
-        </Flex>
-      ) : (
-        <Outlet />
-      )}
-      <UserMenu />
-    </Flex>
+    <Stack>
+      <Topbar />
+      <Flex maxW="large" h="auto" position="relative">
+        {isLoading ? (
+          <Flex justify="center" align="center" height="100vh" width="full">
+            <Spinner size="xl" color="ui.main" />
+          </Flex>
+        ) : (
+          <Outlet />
+        )}
+        {/* <UserMenu /> */}
+      </Flex>
+
+    </Stack>
   )
 }
 
