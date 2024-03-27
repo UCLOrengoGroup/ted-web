9c401617e7a1b67369cc1ae2e87eccc26fac96ec
Ian Sillitoe <ian@sillit.com>
2024-03-27 16:29:02 +0000

    improve handling of search bar

--- frontend/src/routes/_layout/search.tsx
+++ frontend/src/routes/_layout/search.tsx
@@ -5,8 +5,8 @@ import {
   FormErrorMessage,
   Input,
   InputGroup,
-  Flex,
   Link,
+  Flex,
   Heading,
   Spinner,
   Stack,
@@ -14,14 +14,15 @@ import {
   TableContainer,
   Tbody,
   Td,
+  Text,
   Th,
   Thead,
   Tr,
   InputRightAddon
 } from "@chakra-ui/react"
 import React from "react"
-import { createFileRoute } from "@tanstack/react-router"
-import { useQuery } from "react-query"
+import { Link as RouterLink, createFileRoute } from "@tanstack/react-router"
+import { useQuery, useQueryClient } from "react-query"
 import { type SubmitHandler, useForm } from "react-hook-form"
 
 import { type ApiError, UniprotService } from "../../client"
@@ -35,7 +36,7 @@ interface SearchForm {
   query: string
 }
 
-function SearchBar() {
+function SearchBar( {onSubmitQuery}: {onSubmitQuery: (query: string) => void}){
   const {
     register,
     handleSubmit,
@@ -48,11 +49,10 @@ function SearchBar() {
     },
   })
 
-  const [_query, setQuery] = React.useState<string>("")
-
   const onSubmit: SubmitHandler<SearchForm> = (data) => {
-    console.log("data", data)
-    setQuery(data.query)
+    console.log("SearchBar.onSubmit", data)
+    onSubmitQuery(data.query)
+    console.log("SearchBar.onSubmitQuery", onSubmitQuery)
   }
 
   return (
@@ -62,7 +62,7 @@ function SearchBar() {
           <InputGroup>
             <Input
               id="query"
-              placeholder="Enter UniProt ID"
+              placeholder="Enter UniProt ID e.g. A0A001"
               {...register("query", {
                 required: "This field is required",
               })}
@@ -78,28 +78,50 @@ function SearchBar() {
       </form>
     </Stack>
   )
+}
 
+function fetchDomainSummary(query: string) {
+  console.log("fetchDomainSummary.query: ", query)
+  if (!query) {
+    return null
+  }
+  return UniprotService.readUniprotSummary({ uniprotAcc: query, limit: 50 })
 }
 
 function Search() {
   const showToast = useCustomToast()
 
-  const uniprotAcc: string = 'A0A001'
-  const {
-    data: domain_summary_entries,
-    isLoading,
-    isError,
-    error,
-  } = useQuery("domainsummary", () => UniprotService.readUniprotSummary({ uniprotAcc: uniprotAcc, skip: 1, limit: 50 }))
+  const [searchQuery, setSearchQuery] = React.useState<string>("")
+
+  console.log("Search.searchQuery: ", searchQuery)
+
+  const queryClient = useQueryClient()
+  const query = useQuery("search", () => fetchDomainSummary(searchQuery))
+
+  const { data: domain_summary_entries, error, isError, isLoading } = query || {}
+
+  console.log("Search.data: ", domain_summary_entries)
 
   if (isError) {
     const errDetail = (error as ApiError).body?.detail
     showToast("Something went wrong.", `${errDetail}`, "error")
   }
 
-  const uniprot_items = [domain_summary_entries?.data[0]]
-  const uniprot_acc = domain_summary_entries?.data[0].uniprot_acc
-  const ted_count = domain_summary_entries?.count
+  function handleSearchSubmit( _search_query: string) {
+    console.log("Search.handleSearchSubmit: ", _search_query)
+    queryClient.invalidateQueries("search")
+    setSearchQuery(_search_query)
+  }
+
+  function tedToAf(ted_id: string) {
+    return ted_id.substring(0, ted_id.lastIndexOf('_'))
+  }
+
+  const first_entry = domain_summary_entries?.data[0]
+  const uniprot_items = first_entry ? [first_entry] : []
+  const af_id = first_entry ? tedToAf(first_entry.ted_id) : null
+  const uniprot_acc = first_entry?.uniprot_acc
+  const ted_count = domain_summary_entries ? domain_summary_entries.count : 0
 
   return (
     <>
@@ -110,35 +132,47 @@ function Search() {
         </Flex>
       ) : (
         uniprot_items && (
-            <Container maxWidth={"120ch"}>
-              <SearchBar />
+            <Container maxWidth={"100ch"}>
+              <Stack spacing={3}>
+              <SearchBar onSubmitQuery={(query) => {
+                console.log("SearchBar.onSubmitQuery", query)
+                return handleSearchSubmit(query)
+              }}/>
               <Heading
                 size="lg"
                 textAlign={{ base: "center", md: "left" }}
                 pt={12}
               >
-                Search: { uniprotAcc }
+                Search { searchQuery }
               </Heading>
-            <TableContainer>
-              <Table size={{ base: "sm", md: "md" }}>
-                <Thead>
-                  <Tr>
-                    <Th>UniProt</Th>
-                    <Th>TED Domains</Th>
-                    <Th>Link</Th>
-                  </Tr>
-                </Thead>
-                <Tbody>
-                  {uniprot_items.map((_item) => (
-                    <Tr key={uniprot_acc}>
-                      <Td>{uniprot_acc}</Td>
-                      <Td>{ted_count}</Td>
-                      <Td><Link href={`/uniprot/${uniprot_acc}`}>Go</Link></Td>
+              <Text>
+                { uniprot_items.length } AlphaFold entries found ({ted_count} TED domains)
+              </Text>
+              <TableContainer>
+                <Table size={{ base: "sm", md: "md" }}>
+                  <Thead>
+                    <Tr>
+                      <Th>UniProt</Th>
+                      <Th>AlphaFold ID</Th>
+                      <Th>TED Domains</Th>
+                      <Th>Link</Th>
                     </Tr>
-                  ))}
-                </Tbody>
-              </Table>
-            </TableContainer>
+                  </Thead>
+                  <Tbody>
+                    {uniprot_items.map((_item) => (
+                      <Tr key={uniprot_acc}>
+                        <Td>{af_id}</Td>
+                        <Td>{uniprot_acc}</Td>
+                        <Td>{ted_count}</Td>
+                        <Td>
+                          <Button><Link as={RouterLink} to={`/uniprot/${uniprot_acc}`}>Go</Link></Button>
+                        </Td>
+                      </Tr>
+                    ))}
+                  </Tbody>
+                </Table>
+              </TableContainer>
+            </Stack>
           </Container>
         )
       )}
