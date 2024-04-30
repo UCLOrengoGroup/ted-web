d95f2e8c222c4e4a81d68f0326a56d48bed44fc9
Ian Sillitoe <ian@sillit.com>
2024-04-30 11:19:57 +0100

    add alias for Molstar -> molstar/lib

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,4 +5,9 @@ import { defineConfig } from "vite"
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), TanStackRouterVite()],
+  resolve: {
+    alias: {
+      'Molstar': 'molstar/lib'
+    }
+  }
 })
