85312ed1d9270224f2c07854a56036bcd5614124
Ian Sillitoe <ian@sillit.com>
2024-04-30 22:02:19 +0100

    add light style, use cif

--- frontend/src/components/Common/PDBeMolStarWrapper.tsx
+++ frontend/src/components/Common/PDBeMolStarWrapper.tsx
@@ -4,44 +4,49 @@ import { PDBeMolstarPlugin } from "pdbe-molstar/lib"
 see https://webpack.js.org/loaders/sass-loader/ for example.
 create-react-app should support this natively. */
 import { createRef, useEffect } from "react"
+import "molstar/lib/mol-plugin-ui/skin/light.scss";
 
 interface PDBeMolStarWrapperProps {
   afdb: string
+  onInit: (instance: PDBeMolstarPlugin) => void
 }
 
-const PDBeMolStarWrapper: React.FC<PDBeMolStarWrapperProps> = ({ afdb }) => {
+const PDBeMolStarWrapper: React.FC<PDBeMolStarWrapperProps> = ({ afdb, onInit }) => {
   const parent = createRef<HTMLDivElement>()
 
   // In debug mode of react's strict mode, this code will
   // be called twice in a row, which might result in unexpected behavior.
   useEffect(() => {
     function init() {
-      const url = `https://alphafold.ebi.ac.uk/files/${afdb}.pdb`
+      const url = `https://alphafold.ebi.ac.uk/files/${afdb}.cif`
 
-      const viewerInstance = new PDBeMolstarPlugin()
+      const pluginInstance = new PDBeMolstarPlugin()
 
       //Set options (Checkout available options list in the documentation)
       const options = {
         customData: {
           url: url,
-          format: "pdb",
+          format: "cif",
           binary: false,
         },
         alphafoldView: true,
         bgColor: { r: 255, g: 255, b: 255 },
-        // hideCanvasControls: ['selection', 'animation', 'controlToggle', 'controlInfo']
+        selectColor: { r: 0, g: 0, b: 0 },
+        highlightColor: { r: 0, g: 0, b: 0 },
+        hideControls: true,
+        sequencePanel: true,
       }
 
       if (parent.current === null) return
 
       //Call render method to display the 3D view
-      viewerInstance.render(parent.current, options)
+      pluginInstance.render(parent.current, options)
+
+      if (onInit) {
+        onInit(pluginInstance)
+      }
     }
     init()
-    // return () => {
-    //   window.molstar?.dispose()
-    //   window.molstar = undefined
-    // }
   }, [])
 
   return <div ref={parent} style={{ width: 640, height: 480 }} />
