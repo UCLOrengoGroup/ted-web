21836082a4e0d1c2eff38cc5963bf0577177f41e
Ian Sillitoe <ian@sillit.com>
2024-05-23 16:38:03 +0100

    add check for correct residue labels

--- backend/app/tests/api/routes/test_domain_pdb.py
+++ backend/app/tests/api/routes/test_domain_pdb.py
@@ -11,17 +11,11 @@ from app.tests.utils.uniprot_summary import (
 
 logger = getLogger(__name__)
 
-def test_read_domain_pdb(
-    client: TestClient,
-    db: Session,
-    mock_af_pdb: None
-) -> None:
+
+def test_read_domain_pdb(client: TestClient, db: Session, mock_af_pdb: None) -> None:
 
     uniprot_acc = "P12345"
     chopping = "1-100_200-250"
-
-    logger.info("mock_af_pdb: {mock_af_pdb}")
-    logger.info("mock_af_pdb: {app.}")
 
     dom_sum = create_random_domain_summary(uniprot_acc=uniprot_acc, chopping=chopping)
     db.add(dom_sum)
@@ -39,10 +33,22 @@ def test_read_domain_pdb(
 
     assert "ATOM" in content
 
+    # check that the residue numbers match the chopping
+    residue_labels = []
+    last_residue = None
+    for line in content.split("\n"):
+        if line.startswith("ATOM"):
+            residue_label = line[22:26].strip()
+            if residue_label != last_residue:
+                residue_labels.append(residue_label)
+            last_residue = residue_label
 
-def test_read_domain_pdb_fails_with_404(
-    client: TestClient, db: Session
-) -> None:
+    assert residue_labels == [
+        str(i) for i in list(range(1, 101)) + list(range(200, 251))
+    ]
+
+
+def test_read_domain_pdb_fails_with_404(client: TestClient, db: Session) -> None:
     uni_acc = "A98765"
     ted_id = create_ted_id_string(uni_acc, 1)
     response = client.get(
