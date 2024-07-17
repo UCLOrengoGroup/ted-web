2cd2be6c5e8a569d34f5fa6c14f60ac14e7eb240
Ian Sillitoe <ian@sillit.com>
2024-07-17 21:57:11 +0100

    add interaction

--- frontend/src/routes/_layout/uniprot/$uniprotAcc.tsx
+++ frontend/src/routes/_layout/uniprot/$uniprotAcc.tsx
@@ -1,10 +1,14 @@
 import { useEffect, useState } from "react"
 import {
   Box,
+  Center,
   Container,
   Flex,
   Heading,
   IconButton,
+  Link,
+  List,
+  ListItem,
   Spinner,
   Table,
   TableContainer,
@@ -15,11 +19,12 @@ import {
   Thead,
   Tr,
 } from "@chakra-ui/react"
+import { ExternalLinkIcon } from '@chakra-ui/icons'
 import { ViewIcon } from '@chakra-ui/icons'
 import { createFileRoute } from "@tanstack/react-router"
 import { useQuery } from "react-query"
 
-import { type ApiError, UniprotService, DomainSummary } from "../../../client"
+import { type ApiError, UniprotService, DomainSummaryPublicWithInteractions } from "../../../client"
 import useCustomToast from "../../../hooks/useCustomToast"
 
 import PDBeMolStarWrapper from "../../../components/Common/PDBeMolStarWrapper"
@@ -34,6 +39,11 @@ import UniprotEntryDataService from "../../../components/UniprotService/service"
 import { UniprotData } from "../../../components/UniprotService/model"
 import classes from './$uniprotAcc.module.css'
 import ProteinSummaryFigure, { ProteinSummaryFigureProps } from "../../../components/Common/ProteinSummaryFigure"
+
+function ted_domain_to_id(ted_domain: string) {
+  const id_parts = ted_domain.split('_')
+  return id_parts[id_parts.length - 1]
+}
 
 function UniprotAcc() {
   const showToast = useCustomToast()
@@ -74,7 +84,7 @@ function UniprotAcc() {
     }
   }, [uniprotAcc])
 
-  const selectChopping = (dom: DomainSummary) => {
+  const selectChopping = (dom: DomainSummaryPublicWithInteractions) => {
     if (!plugin) return
     const params = getQueryParamsFromChoppingString(dom.chopping)
     setSelectedDomainId(dom.ted_id)
@@ -87,7 +97,7 @@ function UniprotAcc() {
     setSelectedDomainId(null)
   }
 
-  const highlightChopping = (dom: DomainSummary) => {
+  const highlightChopping = (dom: DomainSummaryPublicWithInteractions) => {
     if (!plugin) return
     const params = getQueryParamsFromChoppingString(dom.chopping)
     plugin.visual.highlight({ data: params })
@@ -100,7 +110,7 @@ function UniprotAcc() {
     setHighlightedDomainId(null)
   }
 
-  const toggleSelectChopping = (dom: DomainSummary) => {
+  const toggleSelectChopping = (dom: DomainSummaryPublicWithInteractions) => {
     unselectChopping()
     selectChopping(dom)
   }
@@ -126,8 +136,12 @@ function UniprotAcc() {
 
   let infoTable = null
   if (uniprotEntry) {
-    infoTable = <Box>
+    infoTable = (
+    <Flex>
+    <Box width="70%">
       <dl>
+        <dt>Accession</dt>
+        <dd>{uniprotAcc}</dd>
         <dt>Name</dt>
         <dd>{uniprotEntry.proteinDescription.fullName}</dd>
         <dt>Length</dt>
@@ -135,90 +149,130 @@ function UniprotAcc() {
         <dt>Organism</dt>
         <dd>{uniprotEntry.organism.scientificName}</dd>
         <dt>Lineage</dt>
-        <dd><Text fontSize="sm">{uniprotEntry.organism.lineage.join(" > ")}</Text></dd>
+        <dd><Text>{uniprotEntry.organism.lineage.join(" > ")}</Text></dd>
       </dl>
     </Box>
+    <Box width="30%">
+      <Heading as="h4" size="md" pb="4">External links</Heading>
+      <List>
+        <ListItem>
+          <Link href={`https://alphafold.ebi.ac.uk/entry/${uniprotAcc}`} isExternal>AlphaFold DB <ExternalLinkIcon mx='2px' /></Link>
+        </ListItem>
+        <ListItem>
+          <Link href={`https://www.uniprot.org/uniprotkb/${uniprotAcc}/entry`} isExternal>UniProt-KB <ExternalLinkIcon mx='2px' /></Link>
+        </ListItem>
+        <ListItem>
+          <Link href={`https://www.ebi.ac.uk/interpro/search/text/${uniprotAcc}/`} isExternal>Interpro <ExternalLinkIcon mx='2px' /></Link>
+        </ListItem>
+      </List>
+    </Box>
+    </Flex>
+    )
+  }
+
+  if (isLoading) {
+    return (
+      <Flex justify="center" align="center" height="100vh" width="full">
+        <Spinner size="xl" color="ui.main" />
+      </Flex>
+    )
+  }
+
+  if (!domain_summary_entries) {
+    return (
+      <Flex justify="center" align="center" height="100vh" width="full">
+        <Text>Sorry, no consensus TED domains found for '{uniprotAcc}'</Text>
+      </Flex>
+    )
   }
 
   // console.log("uniprotEntry: ", uniprotEntry)
   return (
     <>
-      {isLoading ? (
-        // TODO: Add skeleton
-        <Flex justify="center" align="center" height="100vh" width="full">
-          <Spinner size="xl" color="ui.main" />
+      <Container maxWidth={"120ch"}>
+        <Heading
+          as="h1"
+          textAlign={{ base: "center", md: "left" }}
+          pt={12}
+        >
+        TED Domains: {uniprotAcc}
+      </Heading>
+
+        {infoTable}
+
+        <Flex height="50vh" position="relative" margin="1em 0">
+          <Box maxW="lg" maxH="sm" id="molstar-view">
+            {afPdbUrl && <PDBeMolStarWrapper 
+              afdb={afId} 
+              onInit={onInitPlugin} />}
+          </Box>
         </Flex>
-      ) : (
-        domain_summary_entries && (
-          <Container maxWidth={"120ch"}>
-            <Heading
-              size="lg"
-              textAlign={{ base: "center", md: "left" }}
-              pt={12}
-            >
-              UniProt: {uniprotAcc}
-            </Heading>
 
-            {infoTable}
+        <Heading as="h2">
+          TED Consensus Domains ({domain_summary_entries.data.length})
+        </Heading>
+        
+        <Center>
+          <Box p="2em">
+            {summaryFigure}
+          </Box>
+        </Center>
 
-            <Flex height="50vh" position="relative" margin="1em 0">
-              <Box maxW="lg" maxH="sm" id="molstar-view">
-                {afPdbUrl && <PDBeMolStarWrapper 
-                  afdb={afId} 
-                  onInit={onInitPlugin} />}
-              </Box>
-            </Flex>
+        <TableContainer>
+          <Table size={{ base: "sm", md: "md" }}>
+            <Thead>
+              <Tr>
+                <Th></Th>
+                <Th>Domain</Th>
+                <Th>Boundaries</Th>
+                <Th>CATH</Th>
+                <Th>Residues</Th>
+                <Th>Av pLDDT</Th>
+                <Th>Packing Density</Th>
+                <Th>Interactions (PAE)</Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              {domain_summary_entries.data.map((item) => (
+                <Tr key={item.ted_id} 
+                    onMouseOver={() => highlightChopping(item)} 
+                    onMouseOut={() => unhighlightChopping()}
+                    onClick={() => toggleSelectChopping(item)}
+                    className={
+                      (selectedDomainId === item.ted_id ? classes.selected : "") + " " + 
+                      (highlightedDomainId === item.ted_id ? classes.highlighted : "")}
+                    >
+                  <Td><IconButton 
+                        aria-label="View Domain"
+                        colorScheme={highlightedDomainId === item.ted_id ? "blue" : "gray"}
+                        onClick={(e) => {e.preventDefault()}}
+                        icon={<ViewIcon/>} /></Td>
+                  <Td>{ted_domain_to_id(item.ted_id)}</Td>
+                  <Td>{item.chopping}</Td>
+                  <Td>{item.cath_label}</Td>
+                  <Td>{item.nres_domain}</Td>
+                  <Td>{item.plddt}</Td>
+                  <Td>{item.packing_density}</Td>
+                  <Td>
+                    <List>
 
-            <Heading size="md" pt={12}>
-              TED Consensus Domains ({domain_summary_entries.data.length})
-            </Heading>
-            
-            <Box margin="1em 0">
-              {summaryFigure}
-            </Box>
-
-            <TableContainer>
-              <Table size={{ base: "sm", md: "md" }}>
-                <Thead>
-                  <Tr>
-                    <Th></Th>
-                    <Th>Boundaries</Th>
-                    <Th>CATH</Th>
-                    <Th>Residues</Th>
-                    <Th>Av pLDDT</Th>
-                    <Th>Packing Density</Th>
-                  </Tr>
-                </Thead>
-                <Tbody>
-                  {domain_summary_entries.data.map((item) => (
-                    <Tr key={item.ted_id} 
-                        onMouseOver={() => highlightChopping(item)} 
-                        onMouseOut={() => unhighlightChopping()}
-                        onClick={() => toggleSelectChopping(item)}
-                        className={
-                          (selectedDomainId === item.ted_id ? classes.selected : "") + " " + 
-                          (highlightedDomainId === item.ted_id ? classes.highlighted : "")}
-                        >
-                      <Td><IconButton 
-                            aria-label="View Domain"
-                            colorScheme={highlightedDomainId === item.ted_id ? "blue" : "gray"}
-                            onClick={(e) => {e.preventDefault()}}
-                            icon={<ViewIcon/>} /></Td>
-                      <Td>{item.chopping}</Td>
-                      <Td>{item.cath_label}</Td>
-                      <Td>{item.nres_domain}</Td>
-                      <Td>{item.plddt}</Td>
-                      <Td>{item.packing_density}</Td>
-                    </Tr>
-                  ))}
-                </Tbody>
-              </Table>
-            </TableContainer>
-          </Container>
-        )
-      )}
+                    { item.interactions?.map((interaction) => {
+                      const pair_id = interaction.ted_id1 === item.ted_id ? interaction.ted_id2 : interaction.ted_id1
+                      return(
+                        <ListItem>{ted_domain_to_id(pair_id)} ({interaction.pae_score})</ListItem>
+                      )
+                    })}
+                    </List>
+                  </Td>
+                </Tr>
+              ))}
+            </Tbody>
+          </Table>
+        </TableContainer> 
+        
+      </Container>
     </>
-  )
+    )
 }
 
 export default UniprotAcc
--- frontend/src/theme.tsx
+++ frontend/src/theme.tsx
@@ -1,4 +1,4 @@
-import { extendTheme, textDecoration } from "@chakra-ui/react"
+import { extendTheme } from "@chakra-ui/react"
 
 const disabledStyles = {
   _disabled: {
