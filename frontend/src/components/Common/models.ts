80e337d3dbbcd5332421420c8bdfb775dec25302
Ian Sillitoe <ian@sillit.com>
2024-07-30 12:22:57 +0100

    add model for AfChainId

--- frontend/src/components/Common/models.ts
+++ frontend/src/components/Common/models.ts
@@ -9,3 +9,26 @@ export interface DomainAnnotation {
   annotation: string
   segments: SegmentAnnotation[]
 }
+
+export class AfChainId {
+  id: string;
+  uniprot_acc: string;
+  version: number;
+  fragment: number;
+
+  constructor(id: string) {
+    // AF-A0A000-F1-model_v4
+    this.id = id;
+    const afid_regex = /^AF-(\w+)-F(\d+)-model_v(\d+)$/;
+    const matches = id.match(afid_regex);
+
+    if (! matches) {
+      throw new Error(`Invalid afid format '${id}'`); // Throw an error if the id format is invalid
+    }
+
+    const [, uniprot_acc, fragment, version] = matches; // Destructure the matched groups
+    this.uniprot_acc = uniprot_acc;
+    this.version = parseInt(version);
+    this.fragment = parseInt(fragment);
+  }
+}
