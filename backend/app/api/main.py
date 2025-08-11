7e4d71bbef6a65603ae27cce0efdf597baa88a33
Ian Sillitoe <i.sillitoe@ucl.ac.uk>
2025-08-11 17:26:54 +0100

    add external tools

--- backend/app/api/main.py
+++ backend/app/api/main.py
@@ -1,10 +1,11 @@
 from fastapi import APIRouter
 
-from app.api.routes import files, items, login, uniprot, users, utils
+from app.api.routes import files, items, login, uniprot, users, utils, external_tools
 
 api_router = APIRouter()
 api_router.include_router(uniprot.router, prefix="/uniprot", tags=["uniprot"])
 api_router.include_router(files.router, prefix="/files", tags=["files"])
+api_router.include_router(external_tools.router, prefix="/tools", tags=["tools"])
 api_router.include_router(login.router, tags=["login"])
 api_router.include_router(users.router, prefix="/users", tags=["users"])
 api_router.include_router(utils.router, prefix="/utils", tags=["utils"])
